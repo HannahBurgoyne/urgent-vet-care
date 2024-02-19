c04665caf57ff770308204088fc8b38944462e5b
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-02-20 12:40:14 +1300

    Initial commit

--- obj/Debug/net8.0/urgent-vet-care.AssemblyInfo.cs
+++ obj/Debug/net8.0/urgent-vet-care.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("urgent-vet-care")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+408cff9a0296aa31e645cc3f2088e03f6fd495c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e37d4501c7c7375a9154bf59a97c7b3099020c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("urgent-vet-care")]
 [assembly: System.Reflection.AssemblyTitleAttribute("urgent-vet-care")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
