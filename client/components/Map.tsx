b0481a59827c42f08b84af9ac5d35326e2926b25
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-06-18 17:29:17 +1200

    Add a map sandbox into map component

--- client/components/Map.tsx
+++ client/components/Map.tsx
@@ -1,18 +1,39 @@
 import { useThemeColor } from '@/hooks/useThemeColor'
 import { setBackgroundColorAsync } from 'expo-system-ui'
-import { ImageBackground } from 'react-native'
+import { ImageBackground, SafeAreaView, View, StyleSheet } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
 
 export default function Map() {
+  const initialRegion = {
+    latitude: 37.78825, // Initial latitude
+    longitude: -122.4324, // Initial longitude
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }
   return (
-    <div
-      style={{
-        backgroundColor: 'red',
-        display: 'flex',
-        flexDirection: 'column',
-        flexGrow: 0.5,
-      }}
-    >
-      <h1>This is the map component</h1>
-    </div>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <MapView style={styles.map} initialRegion={initialRegion}>
+          <Marker
+            coordinate={{
+              latitude: initialRegion.latitude,
+              longitude: initialRegion.longitude,
+            }}
+            title="Your Location"
+          />
+        </MapView>
+      </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+})
