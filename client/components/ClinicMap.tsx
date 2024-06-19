1214781ea4d5c193586538b793f22e7ee3c58195
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-06-19 16:37:38 +1200

    Protect API key

--- client/components/ClinicMap.tsx
+++ client/components/ClinicMap.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { APIProvider, Map } from '@vis.gl/react-google-maps'
+import { API_KEY } from '@env'
 
 export default function ClinicMap() {
   return (
-    <APIProvider apiKey="AIzaSyBgTBFi5c5Zsoa6pdaybU3DZHldBlAYnh0">
+    <APIProvider apiKey={API_KEY}>
       <Map
         style={{ width: '100vw', height: '100vh' }}
         defaultCenter={{ lat: 22.54992, lng: 0 }}
---
+++ client/env.d.tsx
@@ -0,0 +1,6 @@
+declare module '@env' {
+  export const API_KEY: string
+  export const APP_MAJOR_VERSION: string
+  export const APP_MINOR_VERSION: string
+  export const APP_PATCH_VERSION: string
+}
