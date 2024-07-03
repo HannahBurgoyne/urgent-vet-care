13b55e559095c931900240aadfc8ec0f27cfe68f
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-03 16:49:40 +1200

    begin rendering clinics as map markers

--- client/components/ClinicMapPhone.tsx
+++ client/components/ClinicMapPhone.tsx
@@ -1,11 +1,18 @@
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import { StyleSheet, View, Dimensions, Text } from 'react-native'
 import * as Location from 'expo-location'
 import { useEffect, useState } from 'react'
+import fetchVetClinics from '@/apis/vetclinics'
 
-export default function App() {
+export default function ClinicMapPhone() {
+  // TODO:
+
+  // figure out shape of data from Places API and make TS interface, update useState for vetClinics
+  // Render each vet clinic as a marker on map in user's current location
+
   const [location, setLocation] = useState<null | Location.LocationObject>(null)
   const [errorMsg, setErrorMsg] = useState<null | string>(null)
+  const [vetClinics, setVetClinics] = useState(null)
 
   useEffect(() => {
     ;(async () => {
@@ -22,6 +29,9 @@ export default function App() {
         })
         console.log(location)
         setLocation(location)
+
+        const currentClinics = await fetchVetClinics(location)
+        setVetClinics(currentClinics)
       }
     })()
   }, [])
@@ -48,7 +58,16 @@ export default function App() {
               latitudeDelta: 0.005,
               longitudeDelta: 0.005,
             }}
-          />
+          >
+            {vetClinics && (
+              <Marker
+                coordinate={{
+                  latitude: 35.67714827145542,
+                  longitude: 139.6551462687416,
+                }}
+              />
+            )}
+          </MapView>
         </>
       )}
     </>
