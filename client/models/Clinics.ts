314d90d64fc56a64013d5354d7c22a167cff23e0
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-10-10 17:07:02 +1300

    add interface for clinic details

--- client/models/Clinics.ts
+++ client/models/Clinics.ts
@@ -12,3 +12,31 @@ export interface VetClinic {
 export interface OpeningHours {
   open_now: boolean
 }
+
+export interface ClinicDetails {
+  id: string
+  name: string
+  openingHours: DetailedOpeningHours
+  placeId: string
+  rating: number
+  website: string
+  formattedPhoneNumber: string
+  formattedAddress: string
+}
+
+interface DetailedOpeningHours {
+  openNow: boolean
+  periods: OpeningPeriods[]
+  weekdayText: string[]
+}
+
+interface OpeningPeriods {
+  open: {
+    day: number
+    time: string
+  }
+  close: {
+    day: number
+    time: string
+  }
+}
