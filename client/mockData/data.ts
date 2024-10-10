f922a95f66f1522a31315ef28efc792fc2a87020
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-10-10 16:25:26 +1300

    add fake data for development

---
+++ client/mockData/data.ts
@@ -0,0 +1,311 @@
+export const vetClinics = [
+  [
+    {
+      name: 'Royal Oak Veterinary Care',
+      address: '649 Mount Albert Road, Royal Oak, Auckland',
+      rating: 4.6,
+      location: {
+        lat: -36.9107226,
+        lng: 174.768902,
+      },
+      placeId: 'ChIJsRkNbhxGDW0R1GYJwZDaxIw',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Remuera Veterinary Hospital',
+      address: '228 Orakei Road, Remuera, Auckland',
+      rating: 4.6,
+      location: {
+        lat: -36.862473,
+        lng: 174.8087222,
+      },
+      placeId: 'ChIJeajuYENIDW0R0FKsUqBjntc',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Auckland Pet Hospital',
+      address: '46/48 Pollen Street, Grey Lynn, Auckland',
+      rating: 4.7,
+      location: {
+        lat: -36.8583444,
+        lng: 174.7477194,
+      },
+      placeId: 'ChIJIcep3ZVHDW0R88EyluNXcJI',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Remuera Vets',
+      address: '23 Ascot Avenue, Remuera, Auckland',
+      rating: 4.8,
+      location: {
+        lat: -36.8831024,
+        lng: 174.8017185,
+      },
+      placeId: 'ChIJy-Y5tl9IDW0RTizGblsp5e0',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Balmoral Veterinary Care',
+      address: '482 Dominion Road, Mount Eden, Auckland',
+      rating: 4.6,
+      location: {
+        lat: -36.8834008,
+        lng: 174.7488578,
+      },
+      placeId: 'ChIJ5dQyu0VGDW0Rp8r1WPXSWkQ',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Pet Doctors Support Office',
+      address: '46-48 Pollen Street, Grey Lynn, Auckland',
+      rating: 0,
+      location: {
+        lat: -36.85834449999999,
+        lng: 174.7477119,
+      },
+      placeId: 'ChIJCy1snIxIDW0ReYXEUqVwUOk',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Ellerslie Veterinary Clinic',
+      address: '199 Main Highway, Ellerslie, Auckland',
+      rating: 4.9,
+      location: {
+        lat: -36.9000199,
+        lng: 174.814721,
+      },
+      placeId: 'ChIJW9b6A-JIDW0RnqUcJ7N1Q2s',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Greenlane Veterinary Centre and Cattery',
+      address: '246 Great South Road, Greenlane, Auckland',
+      rating: 4.5,
+      location: {
+        lat: -36.8860531,
+        lng: 174.7886191,
+      },
+      placeId: 'ChIJ7xdheYlIDW0R46XMF7Qqjbs',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Normanby Road Vet Clinic',
+      address: '49 Normanby Road, Mount Eden, Auckland',
+      rating: 4.5,
+      location: {
+        lat: -36.8687003,
+        lng: 174.7645395,
+      },
+      placeId: 'ChIJrfYh4tpHDW0RAXoRv7bw8Vc',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'The Strand Veterinarian',
+      address: 'Textile Centre 1 Kenwyn Street, Parnell, Auckland',
+      rating: 4.7,
+      location: {
+        lat: -36.8505321,
+        lng: 174.7825924,
+      },
+      placeId: 'ChIJPbtydgVIDW0RoLGPPZg9-6w',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'The Vets',
+      address: '608 Manukau Road, Epsom, Auckland',
+      rating: 4.6,
+      location: {
+        lat: -36.90061439999999,
+        lng: 174.7737414,
+      },
+      placeId: 'ChIJu7SJKJ5IDW0RK9EqDzdNRu0',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Pet Doctors St Lukes & Exotics Centre',
+      address: '5 Wagener Place, Mount Albert, Auckland',
+      rating: 4.4,
+      location: {
+        lat: -36.8856567,
+        lng: 174.7306838,
+      },
+      placeId: 'ChIJ3d25U_9GDW0Rlb0BNUTUw7g',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'St Lukes Veterinary Centre',
+      address: '13 Morningside Drive, St Lukes, Auckland',
+      rating: 4.6,
+      location: {
+        lat: -36.87666670000001,
+        lng: 174.7353187,
+      },
+      placeId: 'ChIJYZngBVVGDW0RKkNVJXmW2u0',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Animates Grey Lynn',
+      address: '316 Richmond Road, Grey Lynn, Auckland',
+      rating: 4.5,
+      location: {
+        lat: -36.85528979999999,
+        lng: 174.7310342,
+      },
+      placeId: 'ChIJid2wjXVHDW0RJ-1JpLWiOMA',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Abbotts Way Veterinary Clinic',
+      address: '199 Abbotts Way, Remuera, Auckland',
+      rating: 4.7,
+      location: {
+        lat: -36.8878935,
+        lng: 174.8282144,
+      },
+      placeId: 'ChIJK68-AglJDW0RWOy4g0NUrHM',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Central Bark',
+      address: '99 Saint Georges Bay Road, Parnell, Auckland',
+      rating: 4.3,
+      location: {
+        lat: -36.8518738,
+        lng: 174.7827522,
+      },
+      placeId: 'ChIJQx4m6g9IDW0RsMhxBfM7nRs',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Mt Eden Veterinary Clinic',
+      address: '455A Mount Eden Road, Mount Eden, Auckland',
+      rating: 5,
+      location: {
+        lat: -36.8831888,
+        lng: 174.7615059,
+      },
+      placeId: 'ChIJneJSLYBHDW0RJ9tNJxmPb7E',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'The Skin Vet',
+      address: '1 Kenwyn Street, Parnell, Auckland',
+      rating: 5,
+      location: {
+        lat: -36.8505704,
+        lng: 174.782732,
+      },
+      placeId: 'ChIJK0ss21NJDW0RrBrO6kIaV7s',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Animal Referral Centre (ARC Central)',
+      address: '8 Hereford Street, Freemans Bay, Auckland',
+      rating: 4.8,
+      location: {
+        lat: -36.8582205,
+        lng: 174.7536051,
+      },
+      placeId: 'ChIJKRxyhwBHDW0RJhdNm4sief4',
+      businessStatus: 'OPERATIONAL',
+    },
+    {
+      name: 'Kiwi Vet Behaviour',
+      address: '584 Great South Road, Ellerslie, Auckland',
+      rating: 5,
+      location: {
+        lat: -36.8958803,
+        lng: 174.8013155,
+      },
+      placeId: 'ChIJe19Z9h9JDW0RofvGstu6J5U',
+      businessStatus: 'OPERATIONAL',
+    },
+  ],
+]
+
+export const clinicDetails = [
+  {
+    id: '',
+    name: 'Devonport Veterinary Centre',
+    openingHours: {
+      openNow: true,
+      periods: [
+        {
+          open: {
+            day: 1,
+            time: '0800',
+          },
+          close: {
+            day: 1,
+            time: '1800',
+          },
+        },
+        {
+          open: {
+            day: 2,
+            time: '0800',
+          },
+          close: {
+            day: 2,
+            time: '1800',
+          },
+        },
+        {
+          open: {
+            day: 3,
+            time: '0800',
+          },
+          close: {
+            day: 3,
+            time: '1800',
+          },
+        },
+        {
+          open: {
+            day: 4,
+            time: '0800',
+          },
+          close: {
+            day: 4,
+            time: '1800',
+          },
+        },
+        {
+          open: {
+            day: 5,
+            time: '0800',
+          },
+          close: {
+            day: 5,
+            time: '1800',
+          },
+        },
+        {
+          open: {
+            day: 6,
+            time: '0900',
+          },
+          close: {
+            day: 6,
+            time: '1200',
+          },
+        },
+      ],
+      weekdayText: [
+        'Monday: 8:00\u202fAM – 6:00\u202fPM',
+        'Tuesday: 8:00\u202fAM – 6:00\u202fPM',
+        'Wednesday: 8:00\u202fAM – 6:00\u202fPM',
+        'Thursday: 8:00\u202fAM – 6:00\u202fPM',
+        'Friday: 8:00\u202fAM – 6:00\u202fPM',
+        'Saturday: 9:00\u202fAM – 12:00\u202fPM',
+        'Sunday: Closed',
+      ],
+    },
+    placeId: 'ChIJw8lXsNE3DW0RoOO-tXtcMMQ',
+    rating: 5,
+    website: '',
+    formattedPhoneNumber: '09 445 3206',
+    formattedAddress:
+      '1 Ewen Alison Avenue, Devonport, Auckland 0624, New Zealand',
+  },
+]
