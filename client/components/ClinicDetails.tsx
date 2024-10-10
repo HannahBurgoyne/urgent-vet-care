57f532e93238f4dbcdead94dac225bfb8ff5fe88
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-10-10 17:21:42 +1300

    refactor modal into separate component

---
+++ client/components/ClinicDetails.tsx
@@ -0,0 +1,55 @@
+import { Modal, StyleSheet, Text, View } from 'react-native'
+import { VetClinic } from '@/models/Clinics'
+
+interface Props {
+  closeModal: () => void
+  selectedClinic: VetClinic | null
+}
+
+export default function ClinicDetails({ closeModal, selectedClinic }: Props) {
+  return (
+    <Modal
+      transparent={true}
+      animationType="fade"
+      visible={!!selectedClinic}
+      onRequestClose={closeModal}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalTitle}>{selectedClinic.name}</Text>
+          <Text>Address: {selectedClinic.address}</Text>
+          <Text>Place Id: {selectedClinic.placeId}</Text>
+          <Text style={styles.closeButton} onPress={closeModal}>
+            Close
+          </Text>
+        </View>
+      </View>
+    </Modal>
+  )
+}
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
+  },
+  modalContent: {
+    width: 300,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    elevation: 5,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  closeButton: {
+    color: 'blue',
+    marginTop: 20,
+    textAlign: 'center',
+  },
+})
