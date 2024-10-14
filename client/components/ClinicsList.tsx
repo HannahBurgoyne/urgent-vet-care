cc6d982b0381a9595c639470681789bec06e9e87
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-10-15 11:25:09 +1300

    add a button to save clinic to favourites

--- client/components/ClinicsList.tsx
+++ client/components/ClinicsList.tsx
@@ -1,7 +1,15 @@
 import { fetchClinicDetails } from '@/apis/vetclinics'
 import { ClinicDetails, VetClinic } from '@/models/Clinics'
 import { useEffect, useState } from 'react'
-import { View, Text, FlatList, StyleSheet, Button } from 'react-native'
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 interface Props {
   clinics: VetClinic[]
@@ -39,15 +47,20 @@ export default function ClinicsList({ clinics }: Props) {
         keyExtractor={(item, index) => `${item.placeId}-${index}`}
         renderItem={({ item }) => (
           <View style={styles.container}>
+            <TouchableOpacity onPress={() => saveClinic(item)}>
+              <Icon
+                name="star-o"
+                style={styles.icon}
+                size={24}
+                color="#FFD700"
+              />
+              <Text style={styles.buttonText}>Save to Favourites</Text>
+            </TouchableOpacity>
             <Text>{item.name}</Text>
             <Text>{item.formattedAddress}</Text>
             <Text>{item.formattedPhoneNumber}</Text>
             <Text>{item.website}</Text>
             <Text>{item.openingHours.weekdayText}</Text>
-            <Button
-              onPress={() => saveClinic(item)}
-              title="Save clinic to favourites"
-            />
           </View>
         )}
       />
@@ -56,19 +69,40 @@ export default function ClinicsList({ clinics }: Props) {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 'auto',
+    marginTop: 8,
     flexDirection: 'column',
     bottom: 0,
+    backgroundColor: 'white',
+    borderRadius: 3,
+    padding: 6,
+    margin: 8,
+    shadowColor: 'grey',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 3.84,
+    // Shadow property for Android
+    elevation: 8, // Higher value means a more pronounced shadow
   },
   list: {
-    backgroundColor: 'white',
+    backgroundColor: 'red',
     textAlign: 'left',
   },
-  clinic: {
-    padding: 5,
-    fontWeight: '500',
-    borderColor: 'grey',
-    borderWidth: 1,
-    borderRadius: 1,
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    marginLeft: 5,
+  },
+  icon: {
+    position: 'absolute',
+    top: 10, // Adjust this value as needed
+    right: 10, // Adjust this value as needed
+    zIndex: 1, // Ensures the button is on top
+    marginBottom: 8,
   },
 })
